5888301f1cbc4db4b6be62d63e5bf768838276af
Russell Richardson <admin@russ.network>
2023-03-30 13:16:24 -0400

    Mount LVM bulk-data partition at startup

--- hosts/charlemagne/hardware-configuration.nix
+++ hosts/charlemagne/hardware-configuration.nix
@@ -23,6 +23,11 @@
       fsType = "vfat";
     };
 
+  fileSystems."/mnt/bulk-data" =
+    { device = "/dev/bulk-storage/data";
+      fsType = "ext4";
+    };
+
   swapDevices = [ ];
 
   # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
